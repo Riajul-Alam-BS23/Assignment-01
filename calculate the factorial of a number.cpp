ea812425552e9a7ec7e8e5abe372d75c43c82e25
Riajul Alam BS23 <riajul.alam@brainstation-23.com>
2024-04-25 12:48:13 +0600

    fibonacci code extended in Calculate the factorial of a number.cpp file

--- calculate the factorial of a number.cpp
+++ calculate the factorial of a number.cpp
@@ -7,11 +7,17 @@ int factorial(int n)
         return 1;
     return n * factorial(n - 1);
 }
-
+int fibonacci(int number) {
+    if (number <= 1) {
+        return number;
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
 int main()
 {
     int number = 5;
     int result=factorial(number);
-    cout << "Factorial of "<< num << " is: " << result << endl;
+    cout << "Factorial of "<< number << " is: " << result << endl;
+    cout<< "Fibonacci  of "<<number<<" is: "<< fibonacci(number)<<endl;
     return 0;
 }
